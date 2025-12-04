adf22ad93c1c08e23e9c7a5c136933313c56862b
Emma Holle <contact@emmaholle.work>
2025-12-04 15:41:14 -0700

    wip day 1 part 2

--- day-1/day_1_solution.go
+++ day-1/day_1_solution.go
@@ -1,14 +1,16 @@
 package main
 
-import ("os"; "fmt"; "bufio"; "io"; "strconv")
+import ("os"; "fmt"; "bufio"; "io"; "strconv"; "math")
 
 func main() {
     lines,er:=ParseInput(os.Stdin); if er!=nil { panic(er) }
     // fmt.Println(lines)
     rotations:=CalcLockRotations(lines)
     // fmt.Println(rotations)
-    combo:=CalcLockCombo(rotations)
-    fmt.Println(combo)
+    combo1:=CalcLockCombo(rotations)
+    fmt.Println("Day 1, Part 1 answer: ",combo1)
+    combo2:=CalcLockComboAnyClicks(rotations)
+    fmt.Println("Day 1, Part 2 answer: ",combo2)
 }
 
 func ParseInput(in io.Reader) ([]string,error) {
@@ -43,28 +45,47 @@ func CalcLockCombo(rotations []int) int {
     var zeros int
     start:=50
     sum:=start
-    fmt.Println("start", sum)
+    // fmt.Println("start", sum)
     for _,num:=range rotations {
-//  	new_sum:=sum+num
-//  	if new_sum==0 || new_sum==100 {
-//  	    zeros += 1
-//  	    sum = 0
-//  	}
-//  	if new_sum < 0 {
-//  	    sum = 100 + new_sum
-//  	} else if 100 <= new_sum {
-//  	    sum = new_sum - 100
-// 	} else if new_sum==100 || new_sum==0 {
-// 	    zeros += 1
-// 	    sum = 0
-//  	} else {
-//  	    sum = new_sum
-//  	}
 	sum = (sum + num) % 100
  	if sum==0 {
  	    zeros += 1
  	}
-	fmt.Println(sum)
+	// fmt.Println(sum)
+    }
+    return zeros
+}
+
+func CalcLockComboAnyClicks(rotations []int) int {
+    var (
+	sum	   int
+	zeros	   int
+	clicks	   int
+	pos	   int
+	equo	   int
+	remainder  int
+    )
+    sum	= 50
+    // fmt.Println("Input:\tTotal zeros:\tRaw clicks:\tClicks:\tPosition:")
+    // fmt.Println("num:\tsum:\tzeros:\tfcs:\trd:\tclicks:\tposition:")
+    fmt.Println("num:\tsum:\tremainder:\tzeros:\tclicks:\tposition:")
+    for _,num:=range rotations {
+	// sum    	= sum + num
+        // fcs,rd  :=math.Modf(float64(sum / 100))
+	// clicks 	= int(math.Abs(fcs))
+	// sum    	= sum % 100
+	// pos 	= (sum + 100) % 100
+	// zeros += clicks
+	// fmt.Println(num,"\t",sum,"\t",zeros,"\t",fcs,"\t",rd,"\t",clicks,"\t",pos)
+	equo 	  = int(math.Floor(float64((sum+num)/100)))
+	remainder = (sum + int(math.Abs(float64(num)))) % 100
+	// clicks	  = (sum - int(math.Abs(float64(remainder)))) / 99
+	sum	  = (sum + num) % 100
+	pos 	  = (sum + 100) % 100
+	clicks 	  = (pos + remainder) / 100
+	if remainder==0 && equo==0 { zeros += 1 }
+	zeros 	  += clicks
+	fmt.Println(num,"\t",sum,"\t",remainder,"\t\t",zeros,"\t",clicks,"\t",pos)
     }
     return zeros
 }
