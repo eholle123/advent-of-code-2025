f5da4df6661ec9daf08f075c39d8d148ba743465
Emma Holle <contact@emmaholle.work>
2025-12-04 23:26:40 -0700

    wip day 3 part 2

--- day-3/day-3-solution.go
+++ day-3/day-3-solution.go
@@ -5,13 +5,17 @@ import ("os"; "fmt"; "bufio"; "io"; "strconv")
 func main() {
     lines,er:=ParseInput(os.Stdin); if er!=nil { panic(er) }
     // fmt.Println(lines)
+
     sum:=0
     for i:=range len(lines) {
 	if lines[i]!="" {
 	    sum += GetBatteriesOn(lines[i])
 	}
     }
-    fmt.Println("Day 1 Part 1:",sum)
+    fmt.Println("Day 3 Part 1:",sum)
+
+    sum_12:=TurnOnBig12(lines)
+    fmt.Println("Day 3 Part 2:",sum_12)
 }
 
 func ParseInput(in io.Reader) ([]string,error) {
@@ -25,60 +29,12 @@ func ParseInput(in io.Reader) ([]string,error) {
     return lines,nil
 }
 
-// func GetSum(lines []string) int {
-//     // var (
-//     //     bigs 	[]int
-//     //     b1	int
-//     //     b2	int
-//     // )
-//     // f9,l9,f8,l8,f7,l7,f6,l6,f5,l5,f4,l4
-//     for _,line:=range lines {
-// //	if len(line)<2 { panic("too short") }
-// //	f9:=strings.Index(line,"9")
-// //	l9:=strings.LastIndex(line,"9)
-// //	if f9!=-1 && l9!=-1 && f9!=l9 {}
-// 	// b1 := strconv.Atoi(line[0])
-// 	// b2 := strconv.Atoi(line[1])
-// 	// for i,s:=range line {
-// 	//     if i>1 && strconv.Atoi(s)>b1 {
-// 	// 	b1 = strconv.Atoi(line[0])
-// 	//     	b2 = strconv.Atoi(line[1])
-//  	//     }
-// 	// }
-// 	fmt.Println(FindGreatest(line))
-//     }
-//     return 0
-// }
-
 type Big struct {
     index	int
     num		int
 }
 
 func GetBatteriesOn(s string) int {
-    // f9:=strings.Index(line,"9")
-    // f8:=strings.Index(line,"8")
-    // f7:=strings.Index(line,"7")
-    // f6:=strings.Index(line,"6")
-    // f5:=strings.Index(line,"5")
-    // f4:=strings.Index(line,"4")
-    // f3:=strings.Index(line,"3")
-    // f2:=strings.Index(line,"2")
-    // f1:=strings.Index(line,"1")
-    // l9:=strings.LastIndex(line,"9")
-    // l8:=strings.LastIndex(line,"8")
-    // l7:=strings.LastIndex(line,"7")
-    // l6:=strings.LastIndex(line,"6")
-    // l5:=strings.LastIndex(line,"5")
-    // l4:=strings.LastIndex(line,"4")
-    // l3:=strings.LastIndex(line,"3")
-    // l2:=strings.LastIndex(line,"2")
-    // l1:=strings.LastIndex(line,"1")
-    // if f9!=-1 && l9!=-1 && f9!=l9 {
-    //     return 99
-    // }
-    // ns:=[]string{"9","8","7","6","5","4","3","2","1"}
-    // fmt.Println(ns)
     var b1, b2 Big
     max_i:=len(s)-1
     for i:=range max_i {
@@ -98,4 +54,45 @@ func GetBatteriesOn(s string) int {
     batteries_on,er:=strconv.Atoi(string(s[b1.index])+string(s[b2.index])); if er!=nil { panic(er) }
     // fmt.Println(batteries_on)
     return batteries_on
+}
+
+func TurnOnBig12(lines []string) int {
+    energy_sum:=0
+    for _,line:=range lines {
+	fmt.Println(line)
+	var er error
+	battery:=0
+	if len(line) <  12 {
+	    panic("too few batteries in bank!")
+	} else if len(line) == 12 {
+	    battery,er=strconv.Atoi(line); if er!=nil { panic(er) }
+	} else {
+	    fmt.Println(line[:12])
+	    battery_str:=string(line[:12])
+	    battery,er=strconv.Atoi(battery_str); if er!=nil { panic(er) }
+	    max_i:=len(line)-12
+	    for i:=range max_i {
+		battery_i_str:=string(line[i:i+12])
+		battery_i,er:=strconv.Atoi(battery_i_str); if er!=nil { panic(er) }
+		if battery<battery_i {
+		    battery = battery_i
+		}
+		for j:=range 12 {
+		    // fmt.Println(i+j, max_i)
+		    if i+j<=12 {
+    		        battery_j_str:=string(line[i+j:i+12])
+		        curr_num_str:=string(battery_i_str[i:i+j])+battery_j_str
+		        curr_num,er:=strconv.Atoi(curr_num_str); if er!=nil { panic(er) }
+		        // fmt.Println(curr_num, len(curr_num_str))
+		        if battery<curr_num {
+		            battery = curr_num 
+	    	        }
+		    }
+		}
+	    }
+	}
+        fmt.Println(battery)
+	energy_sum += battery
+    }
+    return energy_sum
 }
