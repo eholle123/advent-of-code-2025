9f4367bd0b761a213b74ae6242113bb9ce5e0075
Emma Holle <contact@emmaholle.work>
2025-12-08 22:30:23 -0700

    wip day 2 part 2

--- day-2/day_2_solution.go
+++ day-2/day_2_solution.go
@@ -7,7 +7,7 @@ func main() {
 	// fmt.Println(id_ranges)
 
 	invalid_ids:=GetInvalidIds(id_ranges)
-	// fmt.Println("invalid_ids:",invalid_ids)
+	fmt.Println("invalid_ids:",invalid_ids)
 
 	sum_invalid:=SumInvalidIds(invalid_ids)
 	fmt.Println("Day 2 Part 1 answer:",sum_invalid)
@@ -33,42 +33,64 @@ func ParseIdRanges(in io.Reader) []IDRange {
 }	
 func GetInvalidIds(id_ranges []IDRange) []string {
 	var invalid_ids []string
-	var c1					string
+	// var c1					string
 	for _,id_range:=range id_ranges {
 		start,er:=strconv.Atoi(id_range.start); if er!=nil { panic(er) }
 		stop ,er:=strconv.Atoi(id_range.stop ); if er!=nil { panic(er) }
 		r:=stop-start+1
 		for i:=range r {
 			id:=strconv.Itoa(start+i)
-			for range len(id)+1 {
-				c1 += string(id[0])
+			// for range len(id)+1 {
+			// 	c1 += string(id[0])
+			// }
+			// check:=c1==id
+			// fmt.Println(id, c1, check, CheckSymmetry(id))
+			if CheckSymmetry(id) {
+				invalid_ids = append(invalid_ids,id)
 			}
-			if len(id)%2==0 {
-				p1:=id[:len(id)/2]
-				p2:=id[len(id)/2:]
-				// fmt.Println("p1:",p1,"p2:",p2)
-				if p1==p2 {
-					invalid_ids = append(invalid_ids,id)
-				}
-			}
+			// if len(id)%2==0 {
+			// 	p1:=id[:len(id)/2]
+			// 	p2:=id[len(id)/2:]
+			// 	// fmt.Println("p1:",p1,"p2:",p2)
+			// 	if p1==p2 {
+			// 		invalid_ids = append(invalid_ids,id)
+			// 	}
+			// }
 		}
 	}
 	return invalid_ids
 }
-func CheckSymmetry(s string, n int) bool {
-	id,er:=strconv.Atoi(s); if er!=nil { panic(er) }
+func CheckSymmetry(s string) bool {
+	// id,er:=strconv.Atoi(s); if er!=nil { panic(er) }
 	for i:=range len(s)+1 {
-		if len(s)%i==0 && i!=0 && i!=1 && len(s)/i!=1 && n==i {
-			var prefix string
-			prefix:=string(s[:len(s)/2])
-			current:=string(s[:len(s)/2])
-			found:=true
-			for found==true {
-				after,found:=strings.CutPrefix(s,prefix)
-
-			}
-		} 
+		if i!=0 {
+			fmt.Println("i",i,"len(s)",len(s),"len(s)%i",len(s)%i,"len(s)/i",len(s)/i)
+			if len(s)%i==0 && len(s)/i!=len(s) {
+				var prefix   string
+				var sub_syms []string
+				if i==1 {
+					prefix = string(s[0])
+				} else {
+					prefix = string(s[:len(s)/i])
+				}
+				fmt.Println("i",i,"prefix",prefix)
+				after,found := strings.CutPrefix(s,prefix)
+				for found==true {
+					sub_syms    = append(sub_syms,prefix)
+					after,found = strings.CutPrefix(after,prefix)
+				}
+				fmt.Println(sub_syms)
+				new_s:=""
+				for j:=range len(sub_syms) {
+					new_s += sub_syms[j]
+				}
+				if after!="" && string(new_s)!=s {
+					return false
+				}
+			} 
+		}
 	}
+	return true
 }
 func SumInvalidIds(ids []string) int {
 	var sum int
