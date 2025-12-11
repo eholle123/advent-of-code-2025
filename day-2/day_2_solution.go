876aa1268909ebbb91ebd02f5a4ec1884658d661
Emma Holle <contact@emmaholle.work>
2025-12-11 13:17:34 -0700

    Day 2 cleaned up

--- day-2/day_2_solution.go
+++ day-2/day_2_solution.go
@@ -6,11 +6,17 @@ func main() {
 	id_ranges:=ParseIdRanges(os.Stdin)
 	// fmt.Println(id_ranges)
 
-	invalid_ids:=GetInvalidIds(id_ranges)
-	fmt.Println("invalid_ids:",invalid_ids)
+	invalid_ids_1:=GetInvalidIds(id_ranges,"Part 1")
+	// fmt.Println("Part 1 invalid_ids:",invalid_ids_1)
 
-	sum_invalid:=SumInvalidIds(invalid_ids)
-	fmt.Println("Day 2 Part 2 answer:",sum_invalid)
+	sum_invalid_1:=SumInvalidIds(invalid_ids_1)
+	fmt.Println("Day 2 Part 1 answer:",sum_invalid_1)
+
+	invalid_ids_2:=GetInvalidIds(id_ranges,"Part 2")
+	// fmt.Println("Part 2 invalid_ids:",invalid_ids_2)
+
+	sum_invalid_2:=SumInvalidIds(invalid_ids_2)
+	fmt.Println("Day 2 Part 2 answer:",sum_invalid_2)
 }
 type IDRange struct {
 	start	string
@@ -35,42 +41,38 @@ func ParseIdRanges(in io.Reader) []IDRange {
 	er:=scanner.Err(); if er!=nil { panic(er) }
 	return id_ranges
 }	
-func GetInvalidIds(id_ranges []IDRange) []string {
+func GetInvalidIds(id_ranges []IDRange, p string) []string {
 	var invalid_ids []string
-	// var c1					string
 	for _,id_range:=range id_ranges {
 		start,er:=strconv.Atoi(id_range.start); if er!=nil { panic(er) }
 		stop ,er:=strconv.Atoi(id_range.stop ); if er!=nil { panic(er) }
 		r:=stop-start+1
-		// fmt.Println(id_range)
 		for i:=range r {
 			id:=strconv.Itoa(start+i)
-			// for range len(id)+1 {
-			// 	c1 += string(id[0])
-			// }
-			// check:=c1==id
-			// fmt.Println(id, c1, check, CheckSymmetry(id))
-			if CheckSymmetry(id) {
+			if p=="Part 1" && CheckSymmetryPart1(id) {
 				invalid_ids = append(invalid_ids,id)
 			}
-			// if len(id)%2==0 {
-			// 	p1:=id[:len(id)/2]
-			// 	p2:=id[len(id)/2:]
-			// 	// fmt.Println("p1:",p1,"p2:",p2)
-			// 	if p1==p2 {
-			// 		invalid_ids = append(invalid_ids,id)
-			// 	}
-			// }
+			if p=="Part 2" && CheckSymmetryPart2(id) {
+				invalid_ids = append(invalid_ids,id)
+			}
 		}
 	}
 	return invalid_ids
 }
-func CheckSymmetry(s string) bool {
-	// id,er:=strconv.Atoi(s); if er!=nil { panic(er) }
+func CheckSymmetryPart1(s string) bool {
+	if len(s)%2==0 {
+		p1:=s[:len(s)/2]
+		p2:=s[len(s)/2:]
+		if p1==p2 {
+			return true
+		}
+	}
+	return false
+}
+func CheckSymmetryPart2(s string) bool {
 	if len(s)>1 {
 		for i:=range len(s)+1 {
 			if i!=0 {
-				// fmt.Println("i",i,"len(s)",len(s),"len(s)%i",len(s)%i,"len(s)/i",len(s)/i)
 				if len(s)%i==0 {
 					var prefix   string
 					var sub_syms []string
@@ -79,7 +81,6 @@ func CheckSymmetry(s string) bool {
 					} else {
 						prefix = string(s[:len(s)/i])
 					}
-					// fmt.Println("i",i,"prefix",prefix)
 					after,found := strings.CutPrefix(s,prefix)
 					sub_syms = append(sub_syms,prefix)
 					for range i-1 {
@@ -87,14 +88,11 @@ func CheckSymmetry(s string) bool {
 						if found {
 							sub_syms = append(sub_syms,prefix)
 						}
-						// after,found = strings.CutPrefix(after,prefix)
 					}
-					// fmt.Println(sub_syms)
 					new_s:=""
 					for j:=range len(sub_syms) {
 						new_s += sub_syms[j]
 					}
-					// fmt.Println(s,new_s,string(new_s)==s)
 					if string(new_s)==s {
 						return true
 					}
@@ -112,7 +110,5 @@ func SumInvalidIds(ids []string) int {
 			sum = sum + num
 		}
 	}
-	// sum1:=11+22+99+111+999+1010+1188511885+222222+446446+38593859+565656+824824824
-	// fmt.Println(sum1)
 	return sum
 }
